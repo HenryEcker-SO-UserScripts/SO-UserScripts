73ae7d72aab0254606712829406015201a43c164
HenryEcker <HenryEcker@users.noreply.github.com>
2022-03-28 00:21:20 -0400

    version bump

--- NLNCommentFinderFlagger.user.js
+++ NLNCommentFinderFlagger.user.js
@@ -3,7 +3,7 @@
 // @description  Find comments which may potentially be no longer needed and flag them for removal
 // @homepage     https://github.com/HenryEcker/SO-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      1.5.5
+// @version      1.5.6
 // @downloadURL  https://github.com/HenryEcker/SO-UserScripts/raw/main/NLNCommentFinderFlagger.user.js
 // @updateURL    https://github.com/HenryEcker/SO-UserScripts/raw/main/NLNCommentFinderFlagger.user.js
 //
