35700f8eeafde09361df36d26f3958f9c6df82ef
HenryEcker <HenryEcker@users.noreply.github.com>
2022-04-30 18:46:35 -0400

    Don't run until SE Ready

--- CommentLinkModifier.user.js
+++ CommentLinkModifier.user.js
@@ -3,7 +3,7 @@
 // @description  Changes comment links to user /posts/comments/:comment_id instead of the standard long link that includes the title
 // @homepage     https://github.com/HenryEcker/SO-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      0.0.1
+// @version      0.0.2
 // @downloadURL  https://github.com/HenryEcker/SO-UserScripts/raw/main/CommentLinkModifier.user.js
 // @updateURL    https://github.com/HenryEcker/SO-UserScripts/raw/main/CommentLinkModifier.user.js
 //
@@ -11,15 +11,16 @@
 // @grant        none
 //
 // ==/UserScript==
-/* globals $ */
+/* globals StackExchange, $ */
 
 (function () {
     'use strict';
-
-    const commentSelector = '.comment-link';
-    const buildNewPath = (commentId) => `/posts/comments/${commentId}`;
-    $(commentSelector).each((idx, elem) => {
-        const jQElem = $(elem);
-        jQElem.attr('href', buildNewPath(jQElem.closest('li').attr('data-comment-id')));
+    StackExchange.ready(() => {
+        const commentSelector = '.comment-link';
+        const buildNewPath = (commentId) => `/posts/comments/${commentId}`;
+        $(commentSelector).each((idx, elem) => {
+            const jQElem = $(elem);
+            jQElem.attr('href', buildNewPath(jQElem.closest('li').attr('data-comment-id')));
+        });
     });
 }());
