4e537a49cec22623a10b6d7c84209d2eabbfa8f0
HenryEcker <HenryEcker@users.noreply.github.com>
2022-05-04 22:19:10 -0400

    Defualt to public search for Stack Overflow

---
+++ DefaultPublicSearch.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Default Search Option to Public
+// @description  Changes the search scope to public by default instead of All
+// @homepage     https://github.com/HenryEcker/SO-UserScripts
+// @author       Henry Ecker (https://github.com/HenryEcker)
+// @version      0.0.1
+// @downloadURL  https://github.com/HenryEcker/SO-UserScripts/raw/main/DefaultPublicSearch.user.js
+// @updateURL    https://github.com/HenryEcker/SO-UserScripts/raw/main/DefaultPublicSearch.user.js
+//
+// @match        *://*stackoverflow.com/*
+// @grant        none
+//
+// ==/UserScript==
+/* globals StackExchange, $ */
+
+(function () {
+    'use strict';
+
+    StackExchange.ready(() => {
+        const select = $('#search select');
+        if (select) {
+            select.val('Public').change();
+        }
+    });
+}());
