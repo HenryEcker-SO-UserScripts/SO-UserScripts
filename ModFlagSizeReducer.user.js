11e3e81a9ccd52dc5842b4886df9ae8d518b5f9f
Henry Ecker <HenryEcker@users.noreply.github.com>
2022-06-01 12:38:56 -0400

    Typo

--- ModFlagSizeReducer.user.js
+++ ModFlagSizeReducer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ModFlagSizeReducer
-// @description  Tries to makes mod flags smaller where possible
+// @description  Tries to make mod flags smaller where possible
 // @homepage     https://github.com/HenryEcker/SO-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
 // @version      0.0.5
