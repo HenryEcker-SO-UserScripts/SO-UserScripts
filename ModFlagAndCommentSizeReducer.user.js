35f630583337e03d9d99c137bfd8e23c7a8fda35
HenryEcker <HenryEcker@users.noreply.github.com>
2022-06-02 19:19:17 -0400

    Tiered reducers (only run certain reducers if over max length)

--- ModFlagAndCommentSizeReducer.user.js
+++ ModFlagAndCommentSizeReducer.user.js
@@ -3,7 +3,7 @@
 // @description  Tries to make mod flags and comments smaller where possible
 // @homepage     https://github.com/HenryEcker/SO-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      1.0.4
+// @version      1.0.5
 // @downloadURL  https://github.com/HenryEcker/SO-UserScripts/raw/main/ModFlagAndCommentSizeReducer.user.js
 // @updateURL    https://github.com/HenryEcker/SO-UserScripts/raw/main/ModFlagAndCommentSizeReducer.user.js
 //
@@ -21,6 +21,10 @@
 (function () {
     'use strict';
 
+    const config = {
+        postFlagMaxLen: 500,
+        commentMaxLen: 600
+    };
     const selectors = {
         css: {
             flagDialoguePopupClass: 'popup'
@@ -35,33 +39,42 @@
     };
 
     // let ids = new Map();
-    const absoluteLinkPattern = new RegExp(`\\[(.*)]\\((?:${window.location.origin})/([^)]+)\\)`);
-    const bareLinkPattern = new RegExp(`(?<!]\\()${window.location.origin}\\/([qa])\\/(\\d+)(?:\\/\\d+)?`, 'g');
-    const reducers = [
-        // Convert any absolute links to relative links
-        (s) => s.replace(absoluteLinkPattern, '[$1](/$2)'),
-        // Shorten /qa/postid/userid to just /qa/postid
-        (s) => s.replace(/\[(.*)]\(\/([qa])\/(\d+)\/(\d+)?\)/g, '[$1](/$2/$3)'),
-        // Shorten /questions/postid/title to just /q/postid
-        (s) => s.replace(/\[(.*)]\(\/questions\/(\d+)\/[^/#]+\)/g, '[$1](/q/$2)'),
-        // Shorten /questions/questionid/title/answerid#answerid to just /a/answerid
-        (s) => s.replace(/\[(.*)]\(\/questions\/\d+\/[^/]+\/(\d+)#\d+\)/g, '[$1](/a/$2)'),
-        // Convert any bare post links to [1](/qa/postid)
-        (s) => s.replace(bareLinkPattern, '[1](/$1/$2)'),
-        // Enumerate numbered links (Goes back through to renumber any existing short-links when needed
-        (s) => {
-            const ids = new Map();
-            return s.replace(/\[\d+]\(\/([qa])\/(\d+)\)/g, (sub, p1, p2) => {
-                if (!ids.has(p2)) {
-                    ids.set(p2, Math.max(0, ...ids.values()) + 1);
-                }
-                return `[${ids.get(p2)}](/${p1}/${p2})`;
-            });
-        },
-        // Shorten /questions/postid/title#comment[commentid]_[postid] to just /posts/comments/commentid
-        (s) => s.replace(/\[(.*)]\(\/questions\/\d+(?:\/[^/]+|\/[^/]+\/\d+)#comment(\d+)_\d+\)/g, '[$1](/posts/comments/$2)'),
-        // Shorten /users/userid/uname to /users/userid
-        (s) => s.replace(/\[(.*)]\(\/users\/(\d+)\/[^/#]+\)/g, '[$1](/users/$2)')
+    const absoluteLinkPattern = new RegExp(`\\[([^]]*)]\\((?:${window.location.origin})/([^)]+)\\)`);
+    const bareLinkPattern = new RegExp(`(?<!]\\()${window.location.origin}\\/([qa])\\/(\\d+)(\\/\\d+)?`, 'g');
+
+    const reducerTiers = [
+        // Tier One Reducers
+        [
+            // Convert any absolute links to relative links
+            (s) => s.replace(absoluteLinkPattern, '[$1](/$2)'),
+            // Shorten /questions/postid/title to just /q/postid
+            (s) => s.replace(/\[([^]]*)]\(\/questions\/(\d+)\/[^/#]+\)/g, '[$1](/q/$2)'),
+            // Shorten /questions/questionid/title/answerid#answerid to just /a/answerid
+            (s) => s.replace(/\[([^]]*)]\(\/questions\/\d+\/[^/]+\/(\d+)#\d+\)/g, '[$1](/a/$2)'),
+            // Convert any bare post links to [1](/qa/postid/userid)
+            (s) => s.replace(bareLinkPattern, '[1](/$1/$2$3)'),
+            // Enumerate numbered links (Goes back through to renumber any existing short-links when needed
+            (s) => {
+                const ids = new Map();
+                return s.replace(/\[\d+]\(\/([qa])\/(\d+)(\/\d+)?\)/g, (sub, p1, p2, p3) => {
+                    if (!ids.has(p2)) {
+                        ids.set(p2, Math.max(0, ...ids.values()) + 1);
+                    }
+                    return `[${ids.get(p2)}](/${p1}/${p2}${p3 || ''})`;
+                });
+            },
+            // Shorten /questions/postid/title#comment[commentid]_[postid] to just /posts/comments/commentid
+            (s) => s.replace(/\[([^]]*)]\(\/questions\/\d+(?:\/[^/]+|\/[^/]+\/\d+)#comment(\d+)_\d+\)/g, '[$1](/posts/comments/$2)'),
+            // Shorten /users/userid/uname to /users/userid
+            (s) => s.replace(/\[([^]]*)]\(\/users\/(\d+)\/[^/#]+\)/g, '[$1](/users/$2)')
+        ],
+        // Tier Two Reducers
+        [
+            // Shorten /qa/postid/userid to just /qa/postid
+            (s) => s.replace(/\[([^]]*)]\(\/([qa])\/(\d+)\/(\d+)?\)/g, '[$1](/$2/$3)'),
+            // Remove any excess whitespace
+            (s) => s.replace(/\s{2,}/g, ' ')
+        ]
     ];
 
     const posTrackingReplace = (text, reducer, pos) => {
@@ -74,7 +87,7 @@
         return [text, pos];
     };
 
-    const patternReducer = (text, pos) => {
+    const patternReducer = (reducers, text, pos) => {
         return reducers.reduce((
             [newText, newPos], reducer
         ) => posTrackingReplace(newText, reducer, newPos), [text, pos]);
@@ -93,10 +106,18 @@
         );
     };
 
-    const textAreaMonitor = (textArea, cb = undefined) => {
+    const textAreaMonitor = (textArea, maxLen, cb = undefined) => {
         if (textArea.attr(selectors.attrs.monitoredTextArea) !== true) { // prevent adding the listener multiple times
             textArea.on('input propertychange', (ev) => {
-                const [reducedText, selectionStart] = patternReducer(ev.target.value, ev.target.selectionStart);
+                let reducedText = ev.target.value;
+                let selectionStart = ev.target.selectionStart;
+                for (const reducers of reducerTiers) {
+                    let [reducedText, selectionStart] = patternReducer(reducers, reducedText, selectionStart);
+                    // If reducedText is under maxLen stop reducing
+                    if (reducedText.length <= maxLen) {
+                        break;
+                    }
+                }
                 ev.target.value = reducedText;
                 // Fix Cursor Position
                 ev.target.selectionStart = selectionStart;
@@ -105,7 +126,7 @@
                 // Optionally do something else with reducedText and selectionStart
                 if (cb) cb(reducedText, selectionStart);
             });
-            textArea.attr('data-mfacsr-monitored', true);
+            textArea.attr(selectors.attrs.monitoredTextArea, true);
         }
     };
 
@@ -115,7 +136,7 @@
             if (testIsFlagPopup(nodeEvent)) {
                 const textArea = $('textarea[name="otherText"]');
 
-                textAreaMonitor(textArea, (reducedText) => {
+                textAreaMonitor(textArea, config.postFlagMaxLen, (reducedText) => {
                     flagText = reducedText;
                 });
 
@@ -130,7 +151,7 @@
                     textArea.focus();
                 }
             } else if (testIsCommentBox(nodeEvent)) {
-                textAreaMonitor($(nodeEvent.target));
+                textAreaMonitor($(nodeEvent.target), config.commentMaxLen);
             }
         });
     });
