8267e560edfa2b289dfa431e09bce49411460ccb
HenryEcker <HenryEcker@users.noreply.github.com>
2022-06-06 17:11:55 -0400

    Added RAD

---
+++ ReviewAuditDetector.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         ReviewAuditDetector
+// @description  Tries to detect audits when reviewing
+// @homepage     https://github.com/HenryEcker/SO-UserScripts
+// @author       Henry Ecker (https://github.com/HenryEcker)
+// @version      0.0.1
+// @downloadURL  https://github.com/HenryEcker/SO-UserScripts/raw/main/ReviewAuditDetector.user.js
+// @updateURL    https://github.com/HenryEcker/SO-UserScripts/raw/main/ReviewAuditDetector.user.js
+//
+// @match        *://*.stackoverflow.com/review/*
+// @match        *://*.askubuntu.com/review/*
+// @match        *://*.mathoverflow.net/review/*
+// @match        *://*.serverfault.com/review/*
+// @match        *://*.stackapps.com/review/*
+// @match        *://*.stackexchange.com/review/*
+//
+// @exclude      *://*.com/review/*/stats
+// @exclude      *://*.com/review/*/history
+//
+// @run-at       document-start
+//
+// @grant        none
+//
+// ==/UserScript==
+/* globals $ */
+
+(function () {
+
+    'use strict';
+
+    const reviewInstructionQuerySelector = '.js-review-instructions > .s-notice.s-notice__info';
+
+    $(document).on('ajaxComplete', (event, {responseJSON}, {url}) => {
+        if (
+            (
+                url.startsWith('/review/next-task') || url.startsWith('/review/task-reviewed/')
+            ) && responseJSON.reviewTaskId
+        ) {
+            const reviewInstructionBanner = document.querySelector(reviewInstructionQuerySelector);
+            if (responseJSON.isAudit) {
+                reviewInstructionBanner.innerText = '(Audit) ' + reviewInstructionBanner.innerText;
+                reviewInstructionBanner.style.backgroundColor = 'var(--red-200)';
+            }
+        }
+    });
+})();
