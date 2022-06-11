39d54863bd4b932e0fb8fb00028b7186528254ba
HenryEcker <HenryEcker@users.noreply.github.com>
2022-06-11 15:04:44 -0400

    Add Review to matches

--- SEToastSuppressor.user.js
+++ SEToastSuppressor.user.js
@@ -3,7 +3,7 @@
 // @description  Suppress just the "You haven&#39;t voted on questions in a while; questions need votes too!" toast
 // @homepage     https://github.com/HenryEcker/SO-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      0.0.1
+// @version      0.0.2
 // @downloadURL  https://github.com/HenryEcker/SO-UserScripts/raw/main/SEToastSuppressor.user.js
 // @updateURL    https://github.com/HenryEcker/SO-UserScripts/raw/main/SEToastSuppressor.user.js
 //
@@ -14,6 +14,30 @@
 // @match        *://*.stackoverflow.com/questions/*
 // @match        *://*.superuser.com/questions/*
 // @match        *://*.mathoverflow.net/questions/*
+//
+// @match        *://*.askubuntu.com/review/*
+// @match        *://*.serverfault.com/review/*
+// @match        *://*.stackapps.com/review/*
+// @match        *://*.stackexchange.com/review/*
+// @match        *://*.stackoverflow.com/review/*
+// @match        *://*.superuser.com/review/*
+// @match        *://*.mathoverflow.net/review/*
+//
+// @exclude      *://*.askubuntu.com/review/*/stats
+// @exclude      *://*.askubuntu.com/review/*/history
+// @exclude      *://*.serverfault.com/review/*/stats
+// @exclude      *://*.serverfault.com/review/*/history
+// @exclude      *://*.stackapps.com/review/*/stats
+// @exclude      *://*.stackapps.com/review/*/history
+// @exclude      *://*.stackexchange.com/review/*/stats
+// @exclude      *://*.stackexchange.com/review/*/history
+// @exclude      *://*.stackoverflow.com/review/*/stats
+// @exclude      *://*.stackoverflow.com/review/*/history
+// @exclude      *://*.superuser.com/review/*/stats
+// @exclude      *://*.superuser.com/review/*/history
+// @exclude      *://*.mathoverflow.net/review/*/stats
+// @exclude      *://*.mathoverflow.net/review/*/history
+//
 // @grant        none
 //
 // ==/UserScript==
