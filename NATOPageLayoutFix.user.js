176b7f4f1de81b8ac783ec67632c1bc0097daf81
HenryEcker <HenryEcker@users.noreply.github.com>
2022-06-13 23:55:25 -0400

    Inline Edit support

--- NATOPageLayoutFix.user.js
+++ NATOPageLayoutFix.user.js
@@ -3,7 +3,7 @@
 // @description  Makes Layout on NATO page consistent by removing the table structure and replacing it with grid layout. Also add easy VLQ and NAA flag buttons
 // @homepage     https://github.com/HenryEcker/SO-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      0.0.7
+// @version      0.0.9
 // @downloadURL  https://github.com/HenryEcker/SO-UserScripts/raw/main/NATOPageLayoutFix.user.js
 // @updateURL    https://github.com/HenryEcker/SO-UserScripts/raw/main/NATOPageLayoutFix.user.js
 //
@@ -26,12 +26,17 @@
     const config = {
         selector: {
             mainbar: '#mainbar',
-            table: 'table.default-view-post-table'
+            table: 'table.default-view-post-table',
+            answerLink: 'a.answer-hyperlink',
+            postBody: 'div.s-prose.js-post-body',
+            userCard: 'div.user-info'
         },
         css: {
             container: 'grid-nato-display',
             rowCell: 'nato-grid-row',
-            flagControls: 'nato-flag-controls'
+            // needed for inline editing
+            answer: 'answer',
+            answerCell: 'answercell'
         },
         flagNames: {
             NAA: 'AnswerNotAnAnswer',
@@ -45,7 +50,7 @@
         style.innerHTML = `
             .${config.css.container} {
                 display: grid;
-                grid-template-columns: calc(100% - 172px - var(--su-static24)) max-content;
+                grid-template-columns: calc(100% - 172px - var(--su-static24));
                 width: 100% !important;
             }
         
@@ -55,45 +60,68 @@
                 padding-bottom: 10px;
                 overflow: hidden;
             }
-            
-            .${config.css.container} > .${config.css.rowCell} > .${config.css.flagControls} {
-               margin-top: 10px;
-            }
         `;
         document.head.append(style);
     };
     const rebuildNATOLayout = () => {
         $(`${config.selector.mainbar}`).attr('class', config.css.container);
         const table = $(`${config.selector.table}`);
-        for (const node of $(`${config.selector.table} > tbody > tr > td`)) {
-            $(`<div class="${config.css.rowCell}"/>`).html(node.children).insertBefore(table);
-        }
-        table.remove();
-    };
+        $(`${config.selector.table} > tbody > tr`).each((idx, tr) => {
+            const tds = $(tr).find('td');
+            const answerNode = $(tds[0]);
+            const userCard = $(tds[1]).find(config.selector.userCard);
+            userCard.find('a').attr('target', '_blank');
 
-    const buildFlagButtons = () => {
-        $(`.${config.css.rowCell}:nth-child(odd)`).each((i, e) => {
-            const div = $(e);
-            const link = div.find('a');
-            link.attr('target', '_blank');
-            const answerId = link.attr('href').split('#')[1];
-            div.attr('data-answer-id', answerId);
-            div.append($(`<div class="${config.css.flagControls} flex--item mr16" style="flex: 1 1 100px;">
-    <div class="js-post-menu pt2" data-post-id="${answerId}">
-        <div class="d-flex gs8 s-anchors s-anchors__muted fw-wrap">
-            <div class="flex--item">
-                <button type="button" class="js-flag-post-link s-btn s-btn__link"
-                        title="Flag this post for serious problems or moderator attention">
-                    Flag
-                </button>
+            // Answer Link
+            const answerLink = answerNode.find(`> ${config.selector.answerLink}`);
+            answerLink.attr('target', '_blank');
+            const answerId = answerLink.attr('href').split('#')[1];
+            answerNode.attr('data-answer-id', answerId);
+
+            // Build New Container for Answer Body and Answer Controls
+            const answerWrapper = $(`<div class="${config.css.answer}">`);
+            const answerCell = $(`<div class="${config.css.answerCell}"/>`);
+            // Answer Body
+            const answerBody = answerNode.find(`> ${config.selector.postBody}`);
+            answerCell.append(answerBody);
+            const footer = $(`<div class="mt24">
+    <div class="d-flex fw-wrap ai-start jc-end gs8 gsy">
+        <div class="flex--item mr16" style="flex: 1 1 100px;">
+            <div class="js-post-menu pt2" data-post-id="${answerId}">
+                <div class="d-flex gs8 s-anchors s-anchors__muted fw-wrap">
+                    <div class="flex--item">
+                        <a href="/posts/${answerId}/edit" class="js-edit-post"
+                           title="Revise and improve this post">Edit</a>
+                    </div>
+                    <div class="flex--item">
+                        <button type="button" class="js-flag-post-link s-btn s-btn__link"
+                                title="Flag this post for serious problems or moderator attention">
+                            Flag
+                        </button>
+                    </div>
+                </div>
+                <div class="js-menu-popup-container"></div>
             </div>
         </div>
-        <div class="js-menu-popup-container"></div>
+        <div class="post-signature flex--item fl0"></div>
     </div>
-</div>`));
+</div>`);
+            footer.find('div.post-signature').append(userCard);
+            answerCell.append(footer);
+            answerWrapper.append(answerCell);
+
+            // Top Level component
+            const NATOWrapper = $(`<div class="${config.css.rowCell}"/>`);
+            NATOWrapper.append(answerLink);
+            NATOWrapper.append(answerWrapper);
+            // Add to DOM
+            NATOWrapper.insertBefore(table);
         });
+        table.remove();
         // Create Click Listener for Flag Button
         StackExchange.vote_closingAndFlagging.init();
+        // Create inline editor support
+        StackExchange.inlineEditing.init();
     };
 
     const enableCodeSupport = () => {
@@ -109,7 +137,6 @@
     StackExchange.ready(() => {
         buildStyle();
         rebuildNATOLayout();
-        buildFlagButtons();
         enableCodeSupport();
     });
 }());
