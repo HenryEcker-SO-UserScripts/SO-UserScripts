4c1aba4b06b97a6d66a5dd8fc7ee23c3aebed851
HenryEcker <HenryEcker@users.noreply.github.com>
2022-06-22 19:13:23 -0400

    Expand to suppress FancyOverlay Welcome Back

--- SENoticesSuppressor.user.js
+++ SENoticesSuppressor.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         SE Toast Suppressor
-// @description  Suppress annoying toast messages network-wide
+// @name         SE Toast/FancyOverlay Suppressor
+// @description  Suppress annoying toast/overlay messages network-wide
 // @homepage     https://github.com/HenryEcker/SO-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      0.0.6
+// @version      0.0.7
 // @downloadURL  https://github.com/HenryEcker/SO-UserScripts/raw/main/SEToastSuppressor.user.js
 // @updateURL    https://github.com/HenryEcker/SO-UserScripts/raw/main/SEToastSuppressor.user.js
 //
@@ -23,18 +23,32 @@
 (function () {
     'use strict';
 
-    const messagesToSuppress = new Set([
+    const toastMessagesToSuppress = new Set([
         'You haven&#39;t voted on questions in a while; questions need votes too!',
         'Please consider adding a comment if you think this post can be improved.'
     ]);
 
-    StackExchange.ready(() => {
+    const fancyOverlayMessagesToSuppress = new Set([
+        'Welcome back! If you found this question useful,\u003cbr/\u003edon\u0027t forget to vote both the question and the answers up.'
+    ]);
+
+    const addProxies = () => {
         StackExchange.helpers.showToast = new Proxy(StackExchange.helpers.showToast, {
             apply: (target, thisArg, [message, config]) => {
-                if (!messagesToSuppress.has(message)) {
+                if (!toastMessagesToSuppress.has(message)) {
                     target(message, config);
                 }
             }
         });
-    });
+
+        StackExchange.helpers.showFancyOverlay = new Proxy(StackExchange.helpers.showFancyOverlay, {
+            apply: (target, thisArg, [config]) => {
+                if (!fancyOverlayMessagesToSuppress.has(config.message)) {
+                    target(config);
+                }
+            }
+        });
+    };
+
+    addProxies();
 }());
