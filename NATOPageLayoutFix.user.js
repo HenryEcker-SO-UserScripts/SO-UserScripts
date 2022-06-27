2571db44b4272a9509abc11449b8079a0e8a589d
HenryEcker <HenryEcker@users.noreply.github.com>
2022-06-27 14:32:02 -0400

    Prelim voting UI

--- NATOPageLayoutFix.user.js
+++ NATOPageLayoutFix.user.js
@@ -3,7 +3,7 @@
 // @description  Makes Layout on NATO page consistent by removing the table structure and replacing it with grid layout. Also add easy VLQ and NAA flag buttons
 // @homepage     https://github.com/HenryEcker/SO-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      1.0.6
+// @version      1.0.7
 // @downloadURL  https://github.com/HenryEcker/SO-UserScripts/raw/main/NATOPageLayoutFix.user.js
 // @updateURL    https://github.com/HenryEcker/SO-UserScripts/raw/main/NATOPageLayoutFix.user.js
 //
@@ -37,6 +37,8 @@
             rowCell: 'nato-grid-row',
             answerHyperlink: 'answer-hyperlink',
             deletedAnswer: 'deleted-answer',
+            updownvote: 'votecell',
+            questionTitle: 'nato-question-title',
             // needed for inline editing
             answer: 'answer',
             answerCell: 'answercell',
@@ -71,13 +73,29 @@
                 border: var(--nato-fix-border-style);
                 
                 display: grid;
+                grid-template-areas: 'questionTitle questionTitle'
+                                     'questionTime  questionTime'
+                                     'voteComponent answerComponent';
                 grid-template-rows: min-content min-content 1fr;
-                grid-template-columns: 100%;
+                grid-template-columns: 50px calc(100% - 54px);
                 grid-gap: 2px;
+            }
+            
+            .${config.css.rowCell} > .${config.css.updownvote} {
+                grid-area: voteComponent;
+            }
+            
+            .${config.css.rowCell} > .${config.css.answer} {
+                grid-area: answerComponent;
+            }
+            
+            .${config.css.rowCell} > .${config.css.questionTitle} {
+                grid-area: questionTitle;
             }
             
             .${config.css.rowCell} > .${config.css.questionTime} {
                 border-bottom: var(--nato-fix-border-style);
+                grid-area: questionTime;
             }
             
             .${config.css.rowCell} > .${config.css.deletedAnswer} {
@@ -90,17 +108,19 @@
 
     const pullDownAndHighlightPostVotes = async (answerIdSet) => {
         // Highlight Existing Votes
+        let votes = [];
         for (const answerId of answerIdSet) {
             const response = await fetch(`/posts/${answerId}/votes`);
             if (response.status === 200) {
                 const resData = await response.json();
                 // Update UI as we go
-                StackExchange.vote.highlightExistingVotes(resData);
+                votes = [...votes, ...resData];
             }
             await new Promise((resolve) => {
                 return setTimeout(resolve, 75);
             });
         }
+        StackExchange.vote.vote_init(votes);
     };
 
     const rebuildNATOLayout = () => {
@@ -119,9 +139,29 @@
             // Answer Link
             const answerLink = answerNode.find(`> ${config.selector.answerLink}`);
             answerLink.removeClass(config.css.answerHyperlink);
+            answerLink.addClass(config.css.questionTitle);
             answerLink.attr('target', '_blank');
             const answerId = answerLink.attr('href').split('#')[1];
             answerIdSet.add(answerId);
+
+            const voteComponent = $(`<div class="${config.css.updownvote}">
+    <div class="js-voting-container d-flex jc-center fd-column ai-stretch gs4 fc-black-200" data-post-id="${answerId}">
+        <button class="js-vote-up-btn flex--item s-btn s-btn__unset c-pointer " data-controller="s-tooltip" data-s-tooltip-placement="right" aria-pressed="false" aria-label="Up vote" data-selected-classes="fc-theme-primary" data-unselected-classes="" aria-describedby="--stacks-s-tooltip-y2hsdqmt">
+            <svg aria-hidden="true" class="svg-icon iconArrowUpLg" width="36" height="36" viewBox="0 0 36 36"><path d="M2 25h32L18 9 2 25Z"></path></svg>
+        </button>
+        <div id="--stacks-s-tooltip-y2hsdqmt" class="s-popover s-popover__tooltip pe-none wmx2" aria-hidden="true" role="tooltip" style="">This answer is useful<div class="s-popover--arrow" style=""></div></div>
+        <div class="js-vote-count flex--item d-flex fd-column ai-center fc-black-500 fs-title c-pointer" itemprop="upvoteCount" data-value="0" role="button" tabindex="0" data-s-tooltip-placement="right" data-controller="null s-tooltip" aria-describedby="--stacks-s-tooltip-40anz2cd">
+            ?
+        </div>
+        <div id="--stacks-s-tooltip-40anz2cd" class="s-popover s-popover__tooltip pe-none wmx2" aria-hidden="true" role="tooltip" style="">View upvote and downvote totals.<div class="s-popover--arrow" style=""></div></div>
+        <button class="js-vote-down-btn flex--item s-btn s-btn__unset c-pointer " data-controller="s-tooltip" data-s-tooltip-placement="right" aria-pressed="false" aria-label="Down vote" data-selected-classes="fc-theme-primary" data-unselected-classes="" aria-describedby="--stacks-s-tooltip-ffktvdj5">
+            <svg aria-hidden="true" class="svg-icon iconArrowDownLg" width="36" height="36" viewBox="0 0 36 36"><path d="M2 11h32L18 27 2 11Z"></path></svg>
+        </button><div id="--stacks-s-tooltip-ffktvdj5" class="s-popover s-popover__tooltip pe-none" aria-hidden="true" role="tooltip">This answer is not useful<div class="s-popover--arrow"></div></div>
+        <a class="js-post-issue flex--item s-btn s-btn__unset c-pointer py6 mx-auto" href="/posts/${answerId}/timeline" data-shortcut="T" data-ks-title="timeline" data-controller="s-tooltip" data-s-tooltip-placement="right" aria-label="Timeline" aria-describedby="--stacks-s-tooltip-dzfovtel"><svg aria-hidden="true" class="mln2 mr0 svg-icon iconHistory" width="19" height="18" viewBox="0 0 19 18"><path d="M3 9a8 8 0 1 1 3.73 6.77L8.2 14.3A6 6 0 1 0 5 9l3.01-.01-4 4-4-4h3L3 9Zm7-4h1.01L11 9.36l3.22 2.1-.6.93L10 10V5Z"></path></svg></a><div id="--stacks-s-tooltip-dzfovtel" class="s-popover s-popover__tooltip pe-none" aria-hidden="true" role="tooltip">Show activity on this post.<div class="s-popover--arrow"></div></div>
+
+    </div>
+
+</div>`);
 
             // Build New Container for Answer Body and Answer Controls
             const answerWrapper = $(`<div class="${config.css.answer}"  data-answerid="${answerId}">`);
@@ -195,6 +235,7 @@
             const NATOWrapper = $(`<div class="${config.css.rowCell}"/>`);
             NATOWrapper.append(answerLink);
             NATOWrapper.append(questionTime);
+            NATOWrapper.append(voteComponent);
             NATOWrapper.append(answerWrapper);
             // Add to DOM
             NATOWrapper.insertBefore(table);
@@ -208,6 +249,9 @@
         StackExchange.vote.follow_init();
         // Create Click Listener for Flag Button
         StackExchange.vote_closingAndFlagging.init();
+
+        // Enable Fetch Vote Counts
+        StackExchange.vote.bindFetchVoteCounts();
 
         // Background pull votes on the post and highlight (Voting button UIs update: Follow button becomes Following, etc.)
         void pullDownAndHighlightPostVotes(answerIdSet);
