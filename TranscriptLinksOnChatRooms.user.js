d363d317ee8bee69ee9e9f2db18bd78456013894
Henry Ecker <HenryEcker@users.noreply.github.com>
2022-10-24 17:01:24 -0400

    Missing trailing slash

--- TranscriptLinksOnChatRooms.user.js
+++ TranscriptLinksOnChatRooms.user.js
@@ -3,21 +3,21 @@
 // @description  Adds a link directly to the chat transcript on each Chat Room element
 // @homepage     https://github.com/HenryEcker/SO-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      0.0.3
+// @version      0.0.4
 // @downloadURL  https://github.com/HenryEcker/SO-UserScripts/raw/main/TranscriptLinksOnChatRooms.user.js
 // @updateURL    https://github.com/HenryEcker/SO-UserScripts/raw/main/TranscriptLinksOnChatRooms.user.js
 //
-// @match        *://chat.stackoverflow.com
+// @match        *://chat.stackoverflow.com/
 // @match        *://chat.stackoverflow.com/?*
 // @match        *://chat.stackoverflow.com/rooms
 // @match        *://chat.stackoverflow.com/rooms?*
 //
-// @match        *://chat.stackexchange.com
+// @match        *://chat.stackexchange.com/
 // @match        *://chat.stackexchange.com/?*
 // @match        *://chat.stackexchange.com/rooms
 // @match        *://chat.stackexchange.com/rooms?*
 //
-// @match        *://chat.meta.stackexchange.com
+// @match        *://chat.meta.stackexchange.com/
 // @match        *://chat.meta.stackexchange.com/?*
 // @match        *://chat.meta.stackexchange.com/rooms
 // @match        *://chat.meta.stackexchange.com/rooms?*
