c8514005d167d332d99f4d5ed442214bdab4e56c
HenryEcker <HenryEcker@users.noreply.github.com>
2022-10-28 00:24:27 -0400

    Support separate comment and flag reducer tiers

--- ModFlagAndCommentSizeReducer.user.js
+++ ModFlagAndCommentSizeReducer.user.js
@@ -3,7 +3,7 @@
 // @description  Tries to make mod flags and comments smaller where possible
 // @homepage     https://github.com/HenryEcker/SO-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      1.1.6
+// @version      1.1.7
 // @downloadURL  https://github.com/HenryEcker/SO-UserScripts/raw/main/ModFlagAndCommentSizeReducer.user.js
 // @updateURL    https://github.com/HenryEcker/SO-UserScripts/raw/main/ModFlagAndCommentSizeReducer.user.js
 //
@@ -79,90 +79,103 @@
     const bareDomainLink = new RegExp(`(?<!]\\()${window.location.origin}(\\/[\\w#$&+/=?@\\-%]+)`, 'g');
     const absoluteLinkPattern = new RegExp(`\\[(.*?)]\\((?:${window.location.origin})/([^)]+)\\)`, 'g');
 
-    const reducerTiers = [
+    const shortestRelativeLinkReducer = [
+        // Convert any bare link
+        (s) => {
+            return s.replace(bareDomainLink, (sub) => {
+                return `[1](${sub})`;
+            });
+        },
+        // Convert any absolute links to relative links
+        (s) => {
+            return s.replace(absoluteLinkPattern, '[$1](/$2)');
+        },
+        // Shorten /questions/postId/title to just /q/postId
+        (s) => {
+            return s.replace(/\[(.*?)]\(\/questions\/(\d+)\/[^/#]+(?:\?.+?)?\)/g, '[$1](/q/$2)');
+        },
+        // Shorten /questions/questionId/title/answerId#answerId to just /a/answerId
+        (s) => {
+            return s.replace(/\[(.*?)]\(\/questions\/\d+\/.+?#(\d+)(?:\?.+?)?\)/g, '[$1](/a/$2)');
+        },
+        // Shorten /questions/postId/title#comment[commentId]_[postId] to just /posts/comments/commentId
+        (s) => {
+            return s.replace(/\[(.*?)]\(\/questions\/\d+\/.+?#comment(\d+)_\d+\)/g, '[$1](/posts/comments/$2)');
+        },
+        // Shorten /users/userid/uname to /users/userid
+        (s) => {
+            return s.replace(/\[(.*?)]\(\/users\/(\d+)\/[^?]+(\?tab=.+?)?\)/g, (sub, p1, p2, p3) => {
+                if (p3 === undefined || p3 === '?tab=profile') { // profile tab is default
+                    return `[${p1}](/users/${p2})`;
+                }
+                return `[${p1}](/users/${p2}${p3})`;
+            });
+        }
+    ];
+
+    const enumerationReducers = [
+        //----- BARE LINK ENUMERATION ------//
+        // Convert any post links from [1](/qa/postId/userid) to [QA1](/qa/postId/userid)
+        (s) => {
+            return s.replace(/\[\d+]\(\/([qa])\/(\d+)(\/(\d+))?\)/g, (sub, p1, p2, p3) => {
+                return `[${p1.toUpperCase()}1](/${p1}/${p2}${p3 || ''})`;
+            });
+        },
+        // Convert any review links from [1](/review/queueName/reviewId) to [R1](/review/queueName/reviewId)
+        (s) => {
+            return s.replace(/\[1]\((\/review\/.+?\/\d+)\)/g, '[R1]($1)');
+        },
+        // Convert any comment links from [1](/posts/comments/commentId) to [C1](/posts/comments/commentId)
+        (s) => {
+            return s.replace(/\[1]\((\/posts\/comments\/\d+)\)/g, '[C1]($1)');
+        },
+        // Convert any user links from [1](/users/userId) to [U1](/users/userId)
+        (s) => {
+            return s.replace(/\[1]\((\/users\/\d+)\)/g, '[U1]($1)');
+        },
+        // Enumerate numbered links prefixed with link type (QARCU) (Goes back through to renumber any existing short-links when needed)
+        (s) => {
+            const ids = {};
+            return s.replace(/\[([QARCU])\d+]\((\/.+?)\)/g, (sub, p1, p2) => {
+                if (!(p1 in ids)) {
+                    ids[p1] = new Map();
+                    ids[p1].set(p2, 1);
+                } else if (!ids[p1].has(p2)) {
+                    ids[p1].set(p2, Math.max(0, ...ids[p1].values()) + 1);
+                }
+                return `[${p1}${ids[p1].get(p2)}](${p2})`;
+            });
+        }
+    ];
+
+    // Shorten /qa/postId/userid to just /qa/postId
+    const stripUserIdFromQAReducer = (s) => {
+        return s.replace(/\[(.*?)]\(\/([qa])\/(\d+)\/\d+\)/g, '[$1](/$2/$3)');
+    };
+
+    // Further shorten the enumerated links by removing the link type prefix letter and re-enumerating
+    const removeEnumerationPrefixReducer = (s) => {
+        const idMap = new Map();
+        return s.replace(/\[[QARCU]?\d+]\((\/.+?)\)/g, (sub, p1) => {
+            if (!idMap.has(p1)) {
+                idMap.set(p1, Math.max(0, ...idMap.values()) + 1);
+            }
+            return `[${idMap.get(p1)}](${p1})`;
+        });
+    };
+
+    const flagReducerTiers = [
         // Tier One Reducers
-        [
-            // Convert any bare link
-            (s) => {
-                return s.replace(bareDomainLink, (sub) => {
-                    return `[1](${sub})`;
-                });
-            },
-            // Convert any absolute links to relative links
-            (s) => {
-                return s.replace(absoluteLinkPattern, '[$1](/$2)');
-            },
-            // Shorten /questions/postId/title to just /q/postId
-            (s) => {
-                return s.replace(/\[(.*?)]\(\/questions\/(\d+)\/[^/#]+(?:\?.+?)?\)/g, '[$1](/q/$2)');
-            },
-            // Shorten /questions/questionId/title/answerId#answerId to just /a/answerId
-            (s) => {
-                return s.replace(/\[(.*?)]\(\/questions\/\d+\/.+?#(\d+)(?:\?.+?)?\)/g, '[$1](/a/$2)');
-            },
-            // Shorten /questions/postId/title#comment[commentId]_[postId] to just /posts/comments/commentId
-            (s) => {
-                return s.replace(/\[(.*?)]\(\/questions\/\d+\/.+?#comment(\d+)_\d+\)/g, '[$1](/posts/comments/$2)');
-            },
-            // Shorten /users/userid/uname to /users/userid
-            (s) => {
-                return s.replace(/\[(.*?)]\(\/users\/(\d+)\/[^?]+(\?tab=.+?)?\)/g, (sub, p1, p2, p3) => {
-                    if (p3 === undefined || p3 === '?tab=profile') { // profile tab is default
-                        return `[${p1}](/users/${p2})`;
-                    }
-                    return `[${p1}](/users/${p2}${p3})`;
-                });
-            },
-            // Shorten /qa/postId/userid to just /qa/postId
-            (s) => {
-                return s.replace(/\[(.*?)]\(\/([qa])\/(\d+)\/\d+\)/g, '[$1](/$2/$3)');
-            },
-            //----- BARE LINK ENUMERATION ------//
-            // Convert any post links from [1](/qa/postId/userid) to [QA1](/qa/postId/userid)
-            (s) => {
-                return s.replace(/\[\d+]\(\/([qa])\/(\d+)(\/(\d+))?\)/g, (sub, p1, p2, p3) => {
-                    return `[${p1.toUpperCase()}1](/${p1}/${p2}${p3 || ''})`;
-                });
-            },
-            // Convert any review links from [1](/review/queueName/reviewId) to [R1](/review/queueName/reviewId)
-            (s) => {
-                return s.replace(/\[1]\((\/review\/.+?\/\d+)\)/g, '[R1]($1)');
-            },
-            // Convert any comment links from [1](/posts/comments/commentId) to [C1](/posts/comments/commentId)
-            (s) => {
-                return s.replace(/\[1]\((\/posts\/comments\/\d+)\)/g, '[C1]($1)');
-            },
-            // Convert any user links from [1](/users/userId) to [U1](/users/userId)
-            (s) => {
-                return s.replace(/\[1]\((\/users\/\d+)\)/g, '[U1]($1)');
-            },
-            // Enumerate numbered links prefixed with link type (QARCU) (Goes back through to renumber any existing short-links when needed)
-            (s) => {
-                const ids = {};
-                return s.replace(/\[([QARCU])\d+]\((\/.+?)\)/g, (sub, p1, p2) => {
-                    if (!(p1 in ids)) {
-                        ids[p1] = new Map();
-                        ids[p1].set(p2, 1);
-                    } else if (!ids[p1].has(p2)) {
-                        ids[p1].set(p2, Math.max(0, ...ids[p1].values()) + 1);
-                    }
-                    return `[${p1}${ids[p1].get(p2)}](${p2})`;
-                });
-            }
-        ],
+        [...shortestRelativeLinkReducer, stripUserIdFromQAReducer, ...enumerationReducers],
         // Tier Two Reducers
-        [
-            // Further shorten the enumerated links by removing the link type prefix letter and re-enumerating
-            (s) => {
-                const idMap = new Map();
-                return s.replace(/\[[QARCU]?\d+]\((\/.+?)\)/g, (sub, p1) => {
-                    if (!idMap.has(p1)) {
-                        idMap.set(p1, Math.max(0, ...idMap.values()) + 1);
-                    }
-                    return `[${idMap.get(p1)}](${p1})`;
-                });
-            }
-        ]
+        [removeEnumerationPrefixReducer]
+    ];
+
+    const commentReducerTiers = [
+        // Tier One Reducers
+        shortestRelativeLinkReducer,
+        // Tier Two Reducers
+        [stripUserIdFromQAReducer]
     ];
 
     const patternReducer = (reducers, text, pos) => {
@@ -203,7 +216,7 @@
         textArea.attr('rows', 9);
     };
 
-    const textAreaMonitor = (textArea, maxLen, cb = undefined) => {
+    const textAreaMonitor = (textArea, maxLen, reducerTiers, cb = undefined) => {
         if (textArea.attr(selectors.attrs.monitoredTextArea) !== true) { // prevent adding the listener multiple times
             textArea.on('input propertychange', (ev) => {
                 let reducedText = ev.target.value;
@@ -267,7 +280,7 @@
 
                     changeFlagTextAreaSize(textArea);
 
-                    textAreaMonitor(textArea, textAreaMaxLens.postFlagMaxLen, (reducedText) => {
+                    textAreaMonitor(textArea, textAreaMaxLens.postFlagMaxLen, flagReducerTiers, (reducedText) => {
                         flagText = reducedText || undefined;
                     });
 
@@ -294,6 +307,7 @@
                     textAreaMonitor(
                         textArea,
                         textAreaMaxLens.commentFlagMaxLen,
+                        flagReducerTiers,
                         (reducedText) => {
                             commentFlagText = reducedText || undefined;
                         }
@@ -314,7 +328,7 @@
             attachDOMNodeListenerToButton(
                 testIsCommentBox,
                 (nodeEvent) => {
-                    textAreaMonitor($(nodeEvent.target), textAreaMaxLens.commentMaxLen);
+                    textAreaMonitor($(nodeEvent.target), textAreaMaxLens.commentMaxLen, commentReducerTiers);
                 })
         );
     });
