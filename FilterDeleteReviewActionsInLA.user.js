e6b9afa91fb39a929f89f57e050df5adc08ba7f5
Henry Ecker <HenryEcker@users.noreply.github.com>
2022-10-28 17:24:04 -0400

    Highlight clicked links

--- FilterDeleteReviewActionsInLA.user.js
+++ FilterDeleteReviewActionsInLA.user.js
@@ -3,7 +3,7 @@
 // @description  Only shows Delete and Recommend Deletion actions (for posts which are not deleted)
 // @homepage     https://github.com/HenryEcker/SO-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      0.0.1
+// @version      0.0.2
 // @downloadURL  https://github.com/HenryEcker/SO-UserScripts/raw/main/FilterDeleteReviewActionsInLA.user.js
 // @updateURL    https://github.com/HenryEcker/SO-UserScripts/raw/main/FilterDeleteReviewActionsInLA.user.js
 //
@@ -21,8 +21,14 @@
     $('#content table tr:gt(0)').filter((i, n) => {
         const e = $(n);
         const action = e.find('td:eq(2)');
-        // Make task open in new tab
-        action.find('a').attr('target', '_blank');
+
+        // Make link modifications
+        const link = action.find('a');
+        link.attr('target', '_blank');
+        link.on('click', () => {
+            // Keep track of which links have already been clicked
+            link.addClass('bg-red-500');
+        });
 
         const actionText = action.text().trim().toLowerCase();
         const isDeleted = e.find('.iconTrashSm').length > 0;
