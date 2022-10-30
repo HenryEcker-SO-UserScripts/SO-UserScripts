d7c4dd1903f3a49f4913101c9a8df56a52c034d7
Henry Ecker <HenryEcker@users.noreply.github.com>
2022-10-30 14:44:38 -0400

    Add open all button

--- FilterDeleteReviewActionsInLA.user.js
+++ FilterDeleteReviewActionsInLA.user.js
@@ -3,7 +3,7 @@
 // @description  Only shows Delete and Recommend Deletion actions (for posts which are not deleted)
 // @homepage     https://github.com/HenryEcker/SO-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      0.0.3
+// @version      0.0.4
 // @downloadURL  https://github.com/HenryEcker/SO-UserScripts/raw/main/FilterDeleteReviewActionsInLA.user.js
 // @updateURL    https://github.com/HenryEcker/SO-UserScripts/raw/main/FilterDeleteReviewActionsInLA.user.js
 //
@@ -18,7 +18,13 @@
 
 (function () {
     'use strict';
-    $('#content table tr:gt(0)').filter((i, n) => {
+
+    const config = {
+        selector: {rowSelector: '#content table tr:gt(0)'},
+        styles: {visitedStyle: 'bg-red-500'}
+    };
+
+    $(config.selector.rowSelector).filter((i, n) => {
         const e = $(n);
         const action = e.find('td:eq(2)');
 
@@ -27,11 +33,21 @@
         link.attr('target', '_blank');
         link.on('mousedown', () => {
             // Keep track of which links have already been clicked
-            link.addClass('bg-red-500');
+            link.addClass(config.styles.visitedStyle);
         });
 
         const actionText = action.text().trim().toLowerCase();
         const isDeleted = e.find('.iconTrashSm').length > 0;
         return isDeleted || !actionText.contains('delet');
     }).remove();
+
+    const button = $('<button class="s-btn s-btn__xs s-btn__outlined ml6">Open All</button>');
+    button.on('click', (ev) => {
+        ev.preventDefault();
+        for (const e of $(config.selector.rowSelector).find('td:eq(2) a')) {
+            window.open(e.getAttribute('href'), '_blank');
+            $(e).addClass(config.styles.visitedStyle);
+        }
+    });
+    $('#content table tr:eq(0) th:eq(2)').append(button);
 })();
