6f739b650ebb091f16bd5a1c891134c8c60d6e1c
Henry Ecker <HenryEcker@users.noreply.github.com>
2022-12-15 19:18:32 -0500

    Always show flag history after timeline link

--- AlwaysAccessPostRevisions.user.js
+++ AlwaysAccessPostRevisions.user.js
@@ -3,7 +3,7 @@
 // @description  Adds a link to post revisions to all posts voting containers (including posts with no edits)
 // @homepage     https://github.com/HenryEcker/SO-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      0.0.2
+// @version      0.0.3
 // @downloadURL  https://github.com/HenryEcker/SO-UserScripts/raw/main/AlwaysAccessPostRevisions.user.js
 // @updateURL    https://github.com/HenryEcker/SO-UserScripts/raw/main/AlwaysAccessPostRevisions.user.js
 //
@@ -32,7 +32,7 @@
     'use strict';
 
     const config = {
-        votingContainerSelector: '.js-voting-container',
+        votingContainerSelector: '.js-voting-container a[href$="/timeline"]',
         votingContainerButtonClasses: 'flex--item s-btn s-btn__unset c-pointer py6 mx-auto',
         revisionButtonIcon: '<svg aria-hidden="true" class="svg-icon iconDocumentAlt" width="18" height="18" viewBox="0 0 18 18"><path d="M5 3a2 2 0 0 0-2 2v10c0 1.1.9 2 2 2h7a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2H5Zm2 3a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm-2 4.5c0-.28.22-.5.5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5Zm.5 1.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1 0-1ZM5 14.5c0-.28.22-.5.5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5Z"></path><path opacity=".4" d="M5.9 2h6.35A2.75 2.75 0 0 1 15 4.75v9.35c.62-.6 1-1.43 1-2.35v-7.5C16 2.45 14.54 1 12.75 1h-4.5c-.92 0-1.75.38-2.35 1Z"></path></svg>'
     };
@@ -43,8 +43,8 @@
             const postId = e.attr('data-post-id');
             const stacksTooltip = `--stacks-s-tooltip-aapr-${postId}`;
             // Add Link (and icon) to post revisions on all posts
-            $(`<a href="/posts/${postId}/revisions" class="${config.votingContainerButtonClasses}" data-controller="s-tooltip" data-s-tooltip-placement="right" aria-label="Timeline" aria-describedby="${stacksTooltip}">${config.revisionButtonIcon}</a>`).appendTo(e);
-            $(`<div id="${stacksTooltip}" class="s-popover s-popover__tooltip wmx2" role="tooltip">Show post revisions.<div class="s-popover--arrow" style=""></div></div>`).appendTo(e);
+            $(`<div id="${stacksTooltip}" class="s-popover s-popover__tooltip wmx2" role="tooltip">Show post revisions.<div class="s-popover--arrow" style=""></div></div>`).insertAfter(e);
+            $(`<a href="/posts/${postId}/revisions" class="${config.votingContainerButtonClasses}" data-controller="s-tooltip" data-s-tooltip-placement="right" aria-label="Timeline" aria-describedby="${stacksTooltip}">${config.revisionButtonIcon}</a>`).insertAfter(e);
         });
     };
 
