9c0c8b922986cac69f62c9da01fa60c62a91250c
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-02-11 21:57:56 -0500

    Include transcript links on chat user profile current/frequent rooms

--- TranscriptLinksOnChatRooms.user.js
+++ TranscriptLinksOnChatRooms.user.js
@@ -3,7 +3,7 @@
 // @description  Adds a link directly to the chat transcript on each Chat Room element
 // @homepage     https://github.com/HenryEcker/SO-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      0.0.4
+// @version      0.0.5
 // @downloadURL  https://github.com/HenryEcker/SO-UserScripts/raw/main/TranscriptLinksOnChatRooms.user.js
 // @updateURL    https://github.com/HenryEcker/SO-UserScripts/raw/main/TranscriptLinksOnChatRooms.user.js
 //
@@ -11,16 +11,19 @@
 // @match        *://chat.stackoverflow.com/?*
 // @match        *://chat.stackoverflow.com/rooms
 // @match        *://chat.stackoverflow.com/rooms?*
+// @match        *://chat.stackoverflow.com/users/*
 //
 // @match        *://chat.stackexchange.com/
 // @match        *://chat.stackexchange.com/?*
 // @match        *://chat.stackexchange.com/rooms
 // @match        *://chat.stackexchange.com/rooms?*
+// @match        *://chat.stackexchange.com/users/*
 //
 // @match        *://chat.meta.stackexchange.com/
 // @match        *://chat.meta.stackexchange.com/?*
 // @match        *://chat.meta.stackexchange.com/rooms
 // @match        *://chat.meta.stackexchange.com/rooms?*
+// @match        *://chat.meta.stackexchange.com/users/*
 //
 // ==/UserScript==
 /* globals $ */
@@ -44,6 +47,8 @@
     $(document).on('ajaxComplete', (_0, _1, {url}) => {
         // Rebuild on room refresh
         if (url.startsWith('/rooms')) {
+            buildTranscriptLinks();
+        } else if (url.startsWith('/users')) {
             buildTranscriptLinks();
         }
     });
