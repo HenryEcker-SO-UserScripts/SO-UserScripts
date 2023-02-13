3bf1eb6c567d4e1fe3a57bd5022416ee3da503e1
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-02-13 16:49:26 -0500

    Initial commit

---
+++ SuggestedEditRedesign.user.js
@@ -0,0 +1,73 @@
+// ==UserScript==
+// @name         Suggested Edit Redesign
+// @description  A number of very small tweaks to make suggested edits easier to review
+// @homepage     https://github.com/HenryEcker/SO-UserScripts
+// @author       Henry Ecker (https://github.com/HenryEcker)
+// @version      0.0.1
+// @downloadURL  https://github.com/HenryEcker/SO-UserScripts/raw/main/SuggestedEditRedesign.user.js
+// @updateURL    https://github.com/HenryEcker/SO-UserScripts/raw/main/SuggestedEditRedesign.user.js
+//
+// @match        *://*.askubuntu.com/review/suggested-edits*
+// @match        *://*.serverfault.com/review/suggested-edits*
+// @match        *://*.stackapps.com/review/suggested-edits*
+// @match        *://*.stackexchange.com/review/suggested-edits*
+// @match        *://*.stackoverflow.com/review/suggested-edits*
+// @match        *://*.superuser.com/review/suggested-edits*
+// @match        *://*.mathoverflow.net/review/suggested-edits*
+//
+// @exclude      /^https?:\/\/.*((askubuntu|serverfault|stackapps|stackexchange|stackoverflow|superuser)\.com|mathoverflow\.net)\/review\/.*\/(stats|history)/
+//
+// ==/UserScript==
+/* globals $, StackExchange */
+
+(function () {
+    'use strict';
+
+    const config = {
+        postTypeMapping: {
+            1: 'Question',
+            2: 'Answer'
+        }
+    };
+
+
+    function modifyOuterContainerCSS() {
+        $('.container')
+            .css('min-width', '100%')
+            .css('max-width', 'unset');
+    }
+
+    function modifyInnerContainerCSS() {
+        $('#content')
+            .css('margin', '0 auto')
+            .css('width', '100%')
+            .css('max-width', '1500px')
+            .css('padding', '5px 25px');
+    }
+
+    function addPostTypeNotice(postTypeId) {
+        if (postTypeId === undefined || postTypeId === 0) {
+            return;
+        }
+        $('.js-review-task .js-review-content .s-post-summary')
+            .after($(`<div class="fs-display1 ta-center fc-red-800">${config.postTypeMapping[postTypeId]}</div>`));
+    }
+
+
+    function addOnTaskChangeHandler() {
+        $(document).on('ajaxComplete', (_0, {responseJSON}, {url}) => {
+            if (url.startsWith('/review/next-task') || url.startsWith('/review/task-reviewed/')) {
+                addPostTypeNotice(responseJSON.postTypeId);
+            }
+        });
+    }
+
+
+    function main() {
+        modifyOuterContainerCSS();
+        modifyInnerContainerCSS();
+        addOnTaskChangeHandler();
+    }
+
+    StackExchange.ready(main);
+})();
