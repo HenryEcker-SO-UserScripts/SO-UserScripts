b4f8c5292ac94e1fdd8e88caa976bfb78f7af870
Henry Ecker <HenryEcker@users.noreply.github.com>
2024-10-18 19:16:28 -0400

    Add links minis on users' recent pages

--- TranscriptLinksOnChatRooms.user.js
+++ TranscriptLinksOnChatRooms.user.js
@@ -3,7 +3,7 @@
 // @description  Adds a link directly to the chat transcript on each Chat Room element
 // @homepage     https://github.com/HenryEcker-SO-UserScripts/SO-UserScripts
 // @author       Henry Ecker (https://github.com/HenryEcker)
-// @version      0.0.6
+// @version      0.1.0
 // @downloadURL  https://github.com/HenryEcker-SO-UserScripts/SO-UserScripts/raw/main/TranscriptLinksOnChatRooms.user.js
 // @updateURL    https://github.com/HenryEcker-SO-UserScripts/SO-UserScripts/raw/main/TranscriptLinksOnChatRooms.user.js
 //
@@ -31,27 +31,54 @@
 (function () {
     'use strict';
 
-    const buildTranscriptLinks = () => {
+
+    const $createBaseTranscriptLink = (roomId) => {
+        return $(`<a href="/transcript/${roomId}">transcript</a>`);
+    };
+
+    const addTranscriptLinksToRooms = () => {
         $('.room-info-link').each((i, n) => {
-            const e = $(n);
+            const $e = $(n);
+
             // Place corresponding transcript link at the end of each room-info-link container
-            $(`<a href="/transcript/${
-                // Find Room Id from div
-                e.parent('div').attr('id').split('-')[1]
-            }" style="margin-left: 7px;">transcript</a>`).appendTo(e);
+            const roomId = $e.parent('div').attr('id').split('-')[1];
+
+            // Build Transcript Object
+            $createBaseTranscriptLink(roomId)
+                .css({'margin-left': '7px'})
+                .appendTo($e);
+
             // Reduce left offset to accommodate transcript label
-            e.css({'left': '70px', 'width': 'unset'});
+            $e.css({'left': '70px', 'width': 'unset'});
         });
+    };
+
+
+    const addTranscriptLinksToMiniRooms = () => {
+        $('.room-mini-header').each((i, n) => {
+            const $e = $(n);
+            // Get Room id from room name link
+            const roomId = Number(/^\/rooms\/(\d+)/.exec($e.find('.room-name a').attr('href'))[1]);
+            $e.append(
+                $createBaseTranscriptLink(roomId)
+                    .css({'display': 'block', 'margin-top': '7px'})
+            );
+        });
+    };
+
+    const addTranscriptLinks = () => {
+        addTranscriptLinksToRooms();
+        addTranscriptLinksToMiniRooms();
     };
 
     $(document).on('ajaxComplete', (_0, _1, {url}) => {
         // Rebuild on room refresh
         if (url.startsWith('/rooms')) {
-            buildTranscriptLinks();
+            addTranscriptLinks();
         } else if (url.startsWith('/users')) {
-            buildTranscriptLinks();
+            addTranscriptLinks();
         }
     });
 
-    buildTranscriptLinks();
+    addTranscriptLinks();
 })();
